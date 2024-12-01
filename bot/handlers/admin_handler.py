1f183508a3725851969326890ff4af577d2422e0
iswhoami <iswhoamirus@gmail.com>
2024-12-01 17:07:40 +0300

    Инициализация гита

--- bot/handlers/admin_handler.py
+++ bot/handlers/admin_handler.py
@@ -5,6 +5,6 @@ from app import dp, bot
 from bot.filters import IsAdmin
 
 
-@dp.message(Command('admin'), IsAdmin)
+@dp.message(Command('admin'), IsAdmin())
 async def menu_admin_handler(message: types.Message):
     await bot.send_message(chat_id=message.chat.id, text='Я внутри admin_handler')
